3e2c471299badcbde7e61325f9de6820c6e6fefd
Christine Chaewon Shin <chshin0225@gmail.com>
2023-07-15 18:11:50 +0900

    feat: add memo delete feature and change ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -18,9 +19,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const MemoListPage(title: 'My Memos'),
-        '/create': (context) => const MemoCreatePage(title: 'Create Memo'),
-        '/detail': (context) => const MemoDetailPage(title: 'Memo'),
+        '/': (context) => const MemoListPage(title: 'Things to do'),
       },
     );
   }
@@ -29,88 +28,78 @@ class MyApp extends StatelessWidget {
 class MemoListPage extends StatefulWidget {
   const MemoListPage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MemoListPage> createState() => _MemoListPageState();
 }
 
+class Memo {
+  String content;
+  Color color;
+
+  Memo(this.content, this.color);
+}
+
 class _MemoListPageState extends State<MemoListPage> {
-  List<String> memos = [];
+  List<Memo> memos = [];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: ListView.builder(
+      body: GridView.builder(
+        padding: const EdgeInsets.all(12),
         itemCount: memos.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1 / 1,
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+        ),
         itemBuilder: (context, index) {
           return Card(
-            color: Colors.blue[50],
-            child: ListTile(
-              title: Text(memos[index], style: TextStyle(fontSize: 22),),
+            color: memos[index].color,
+            child: Stack(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Center(
+                        child: Text(
+                          memos[index].content, 
+                          style: TextStyle(fontSize: 22), 
+                          textAlign: TextAlign.center,
+                        )
+                      ),
+                    ),
+                  ],
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        memos.removeAt(index);
+                      });
+                      
+                    }, 
+                    icon: Icon(Icons.close),
+                    alignment: Alignment.topRight,
+                  ),
+                ),
+              ]
             ),
           );
         },
       ),
-      // body: ListView(
-      //   padding: const EdgeInsets.all(8),
-      //   children: <Widget>[
-      //     Card(
-      //       color: Colors.blue[50],
-      //       child: const ListTile(
-      //         title: Text('item 1', style: TextStyle(fontSize: 22),),
-      //         subtitle: Text('item 1 content'),
-      //         trailing: Icon(Icons.more_vert),
-      //       ),
-      //     ),
-      //     Card(
-      //       color: Colors.blue[100],
-      //       child: const ListTile(
-      //         title: Text('item 2', style: TextStyle(fontSize: 22),),
-      //         subtitle: Text('item 2 content'),
-      //         trailing: Icon(Icons.more_vert),
-      //       ),
-      //     ),
-      //     Card(
-      //       color: Colors.blue[200],
-      //       child: const ListTile(
-      //         title: Text('item 3', style: TextStyle(fontSize: 22),),
-      //         subtitle: Text('item 3 content'),
-      //         trailing: Icon(Icons.more_vert),
-      //       ),
-      //     ),
-      //     Card(
-      //       color: Colors.blue[300],
-      //       child: const ListTile(
-      //         title: Text('item 4', style: TextStyle(fontSize: 22),),
-      //         subtitle: Text('item 4 content'),
-      //         trailing: Icon(Icons.more_vert),
-      //       ),
-      //     ),
-      //   ],
-      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Navigator.pushNamed(context, '/create');
           _addMemo(context);
         },
         backgroundColor: Colors.blue[700],
@@ -119,14 +108,27 @@ class _MemoListPageState extends State<MemoListPage> {
     );
   }
 
+  Color _selectRandomColor() {
+    List<Color> colors = [Colors.red[50]!, Colors.orange[50]!, Colors.yellow[50]!, Colors.green[50]!, Colors.blue[50]!, Colors.purple[50]!];
+
+    Random random = Random();
+
+    int randomIndex = random.nextInt(colors.length);
+
+    Color randomColor = colors[randomIndex];
+
+    return randomColor;
+  }
+
   void _addMemo(BuildContext context) {
     showDialog(
       context: context,
       builder: (context) {
         String newMemo = '';
         return AlertDialog(
-          title: const Text('Add Memo'),
+          title: const Text('What do you need to do?'),
           content: TextField(
+            autofocus: true,
             onChanged: (value) {
               newMemo = value;
             },
@@ -134,9 +136,12 @@ class _MemoListPageState extends State<MemoListPage> {
           actions: [
             TextButton(
               onPressed: () {
+                Memo memo = Memo(newMemo, _selectRandomColor());
+
                 setState(() {
-                  memos.add(newMemo);
+                  memos.add(memo);
                 });
+
                 Navigator.pop(context);
               },
               child: const Text('Add'),
@@ -144,76 +149,6 @@ class _MemoListPageState extends State<MemoListPage> {
           ],
         );
       },
-    );
-  }
-}
-
-class MemoDetailPage extends StatefulWidget {
-  const MemoDetailPage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MemoDetailPage> createState() => _MemoDetailPageState();
-}
-
-class _MemoDetailPageState extends State<MemoDetailPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-    );
-  }
-}
-
-class MemoCreatePage extends StatefulWidget {
-  const MemoCreatePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MemoCreatePage> createState() => _MemoCreatePageState();
-}
-
-class _MemoCreatePageState extends State<MemoCreatePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          children: [
-            SizedBox(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Title',
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 300,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Content',
-                ),
-              ),
-            ),
-            // ElevatedButton(
-            //   onPressed: () {
-            //   },
-            //   child: Text('create memo'),
-            // ),
-          ],
-        ),
-      ),
-      
     );
   }
 }
